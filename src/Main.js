79db9220128e3ec46b95192c437dce5044f715bd
Alfredo Haddad <alfredohaddad@gmail.com>
2023-07-19 16:19:37 +0100

    Adding login logic

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import { initializeApp } from "firebase/app";
 import { getAuth, signInAnonymously } from "firebase/auth";
-import { getDatabase, ref, onValue } from "firebase/database";
+import { getDatabase, ref, onValue, set, get, update } from "firebase/database";
 import { styled } from "@mui/material/styles";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
@@ -16,6 +16,13 @@ import { red } from "@mui/material/colors";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { Grid } from "@mui/material";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
@@ -31,6 +38,8 @@ const ExpandMore = styled((props) => {
 export default function Main() {
   const [expanded, setExpanded] = useState(false);
   const [workouts, setWorkouts] = useState([]);
+  const [isValidUser, setIsValidUser] = useState(false);
+  const [open, setOpen] = useState(false);
   const images = require.context("./Images", true);
   const firebaseConfig = {
     apiKey: "AIzaSyA0dberbHig8XFSbGVjV86wf_rO5Eo1C7c",
@@ -43,150 +52,239 @@ export default function Main() {
   let app = useRef(null);
   let db = useRef(null);
   let wods = useRef(null);
+  let userUid = useRef(null);
+  let userRef = useRef(null);
+  const nameRef = useRef("");
+  const emailRef = useRef("");
 
   useEffect(() => {
     app.current = initializeApp(firebaseConfig);
     db.current = getDatabase(app.current);
-    getWods();
+    login();
   }, []);
 
-  async function getWods() {
-    wods = ref(db.current, `workouts`);
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-    let result = await signInAnonymously(getAuth(app.current));
-    if (result) {
-      console.log("Logged in as:", result.user.uid);
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-      onValue(wods, (snapshot) => {
-        //fires whenever a change occurs
-        let wods = snapshot.val();
-        console.log(wods);
-        setWorkouts(wods);
-      });
+  const submitUserData = () => {
+    setOpen(false);
+    console.log(nameRef.current.value);
+    console.log(emailRef.current.value);
+    update(userRef.current, {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+    });
+  };
+
+  async function login() {
+    const auth = getAuth();
+    const authUser = await signInAnonymously(auth);
+    if (authUser) {
+      let existingUser = localStorage.getItem("wod-user_uid");
+      if (!existingUser) {
+        console.log("New user with uid:", authUser.user.uid);
+        localStorage.setItem("wod-user_uid", authUser.user.uid);
+        userUid.current = authUser.user.uid;
+        userRef.current = ref(db.current, `users/${userUid.current}`);
+        set(userRef.current, {
+          name: "dummy",
+          email: "dummy@dummy.com",
+          id: userUid.current,
+          workouts: "{}",
+        });
+      } else {
+        userRef.current = ref(db.current, `users/${existingUser}`);
+        get(userRef.current)
+          .then((userData) => {
+            if (userData.exists()) {
+              //validate is not dummy data
+              if (userData.val().name.includes("dummy")) {
+                //we need to show the modal to get user data
+              } else {
+                getWods();
+              }
+            } else {
+              console.log("No data available");
+            }
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      }
     }
-    //   playerId.current = result.user.uid;
-    //   playerRef.current = ref(db.current, `players/${playerId.current}`);
-    //   set(playerRef.current, {
-    //     name: "dummy",
-    //     id: playerId.current,
-    //   });
+  }
+
+  async function getWods() {
+    wods = ref(db, `workouts`);
+
+    onValue(wods, (snapshot) => {
+      //fires whenever a change occurs
+      let wods = snapshot.val();
+      console.log(wods);
+      setWorkouts(wods);
+    });
   }
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
 
-  return (
-    <Grid container spacing={1} justifyContent="center">
-      {workouts.map((wod) => {
-        const { title, description, workout } = wod;
-        return (
-          <Grid item>
-            <Card sx={{ width: 300 }}>
-              <CardHeader
-                avatar={
-                  <Avatar sx={{ bgcolor: red[900] }} aria-label="recipe">
-                    AH
-                  </Avatar>
-                }
-                action={
-                  <IconButton aria-label="add to favorites">
-                    <FavoriteIcon />
-                  </IconButton>
-                }
-                title={title}
-              />
-              <CardMedia
-                component="img"
-                height="5"
-                image={images(`./bg.png`)}
-                alt="Paella dish"
-              />
-              <CardContent>
-                <Typography variant="body1" color="text.primary" align="center">
-                  {description}
-                </Typography>
-                {workout.map(function (movement) {
-                  if (movement.effort)
-                    return (
+  if (isValidUser) {
+    return (
+      <Grid container spacing={1} justifyContent="center">
+        {workouts.map((wod) => {
+          const { title, description, workout } = wod;
+          return (
+            <Grid item>
+              <Card sx={{ width: 300 }}>
+                <CardHeader
+                  avatar={
+                    <Avatar sx={{ bgcolor: red[900] }} aria-label="recipe">
+                      AH
+                    </Avatar>
+                  }
+                  action={
+                    <IconButton aria-label="add to favorites">
+                      <FavoriteIcon />
+                    </IconButton>
+                  }
+                  title={title}
+                />
+                <CardMedia
+                  component="img"
+                  height="5"
+                  image={images(`./bg.png`)}
+                  alt="Paella dish"
+                />
+                <CardContent>
+                  <Typography
+                    variant="body1"
+                    color="text.primary"
+                    align="center"
+                  >
+                    {description}
+                  </Typography>
+                  {workout.map(function (movement) {
+                    if (movement.effort)
+                      return (
+                        <Typography
+                          variant="body1"
+                          color="text.primary"
+                          align="center"
+                        >
+                          {movement.effort}
+                        </Typography>
+                      );
+                    return wod.type === "ladder" ? (
                       <Typography
                         variant="body1"
                         color="text.primary"
                         align="center"
                       >
-                        {movement.effort}
+                        {`${movement.type} ${
+                          movement.weight ? `@${movement.weight}` : ""
+                        }`}
+                      </Typography>
+                    ) : (
+                      <Typography
+                        variant="body1"
+                        color="text.primary"
+                        align="center"
+                      >
+                        {`${movement.reps} ${movement.type} ${
+                          movement.weight ? `@${movement.weight}` : ""
+                        }`}
                       </Typography>
                     );
-                  return wod.type === "ladder" ? (
-                    <Typography
-                      variant="body1"
-                      color="text.primary"
-                      align="center"
-                    >
-                      {`${movement.type} ${
-                        movement.weight ? `@${movement.weight}` : ""
-                      }`}
-                    </Typography>
-                  ) : (
-                    <Typography
-                      variant="body1"
-                      color="text.primary"
-                      align="center"
-                    >
-                      {`${movement.reps} ${movement.type} ${
-                        movement.weight ? `@${movement.weight}` : ""
-                      }`}
-                    </Typography>
-                  );
-                })}
-              </CardContent>
-              <CardActions disableSpacing>
-                <ExpandMore
-                  expand={expanded}
-                  onClick={handleExpandClick}
-                  aria-expanded={expanded}
-                  aria-label="show more"
-                >
-                  <ExpandMoreIcon />
-                </ExpandMore>
-              </CardActions>
-              <Collapse in={expanded} timeout="auto" unmountOnExit>
-                <CardContent>
-                  <Typography paragraph>Method:</Typography>
-                  <Typography paragraph>
-                    Heat 1/2 cup of the broth in a pot until simmering, add
-                    saffron and set aside for 10 minutes.
-                  </Typography>
-                  <Typography paragraph>
-                    Heat oil in a (14- to 16-inch) paella pan or a large, deep
-                    skillet over medium-high heat. Add chicken, shrimp and
-                    chorizo, and cook, stirring occasionally until lightly
-                    browned, 6 to 8 minutes. Transfer shrimp to a large plate
-                    and set aside, leaving chicken and chorizo in the pan. Add
-                    pimentón, bay leaves, garlic, tomatoes, onion, salt and
-                    pepper, and cook, stirring often until thickened and
-                    fragrant, about 10 minutes. Add saffron broth and remaining
-                    4 1/2 cups chicken broth; bring to a boil.
-                  </Typography>
-                  <Typography paragraph>
-                    Add rice and stir very gently to distribute. Top with
-                    artichokes and peppers, and cook without stirring, until
-                    most of the liquid is absorbed, 15 to 18 minutes. Reduce
-                    heat to medium-low, add reserved shrimp and mussels, tucking
-                    them down into the rice, and cook again without stirring,
-                    until mussels have opened and rice is just tender, 5 to 7
-                    minutes more. (Discard any mussels that don&apos;t open.)
-                  </Typography>
-                  <Typography>
-                    Set aside off of the heat to let rest for 10 minutes, and
-                    then serve.
-                  </Typography>
+                  })}
                 </CardContent>
-              </Collapse>
-            </Card>
-          </Grid>
-        );
-      })}
-    </Grid>
+                <CardActions disableSpacing>
+                  <ExpandMore
+                    expand={expanded}
+                    onClick={handleExpandClick}
+                    aria-expanded={expanded}
+                    aria-label="show more"
+                  >
+                    <ExpandMoreIcon />
+                  </ExpandMore>
+                </CardActions>
+                <Collapse in={expanded} timeout="auto" unmountOnExit>
+                  <CardContent>
+                    <Typography paragraph>Method:</Typography>
+                    <Typography paragraph>
+                      Heat 1/2 cup of the broth in a pot until simmering, add
+                      saffron and set aside for 10 minutes.
+                    </Typography>
+                    <Typography paragraph>
+                      Heat oil in a (14- to 16-inch) paella pan or a large, deep
+                      skillet over medium-high heat.
+                    </Typography>
+                    <Typography paragraph>
+                      Add rice and stir very gently to distribute.
+                    </Typography>
+                    <Typography>
+                      Set aside off of the heat to let rest for 10 minutes, and
+                      then serve.
+                    </Typography>
+                  </CardContent>
+                </Collapse>
+              </Card>
+            </Grid>
+          );
+        })}
+      </Grid>
+    );
+  }
+  return (
+    <div>
+      <Typography align="center" sx={{ m: 10 }}>
+        <Button
+          color="primary"
+          size="large"
+          type="submit"
+          variant="contained"
+          onClick={handleClickOpen}
+        >
+          Login to access the workouts
+        </Button>
+      </Typography>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Subscribe</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To have access to the workouts, please enter your name and email
+            address here.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Name"
+            type="text"
+            fullWidth
+            variant="standard"
+            inputRef={nameRef}
+          />
+          <TextField
+            margin="dense"
+            id="name"
+            label="Email Address"
+            type="email"
+            fullWidth
+            variant="standard"
+            inputRef={emailRef}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={submitUserData}>Subscribe</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
   );
 }
