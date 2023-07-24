0b6650cbf6f129485dca81c12e19cbba3b393a47
Alfredo Haddad <alfredohaddad@gmail.com>
2023-07-24 10:20:34 +0100

    Login to see wods

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,11 @@
 import { useState, useEffect, useRef } from "react";
 import { initializeApp } from "firebase/app";
 import { getAuth, signInAnonymously } from "firebase/auth";
-import { getDatabase, ref, onValue, set, get, update } from "firebase/database";
+import { getDatabase, ref, onValue, set, get } from "firebase/database";
 import { styled } from "@mui/material/styles";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
+// import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
 import Collapse from "@mui/material/Collapse";
@@ -23,6 +23,9 @@ import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
+import Divider from "@mui/material/Divider";
+import LinearProgress from "@mui/material/LinearProgress";
+import Box from "@mui/material/Box";
 
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
@@ -38,9 +41,10 @@ const ExpandMore = styled((props) => {
 export default function Main() {
   const [expanded, setExpanded] = useState(false);
   const [workouts, setWorkouts] = useState([]);
-  const [isValidUser, setIsValidUser] = useState(false);
+  const [isExistingUser, setIsExistingUser] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [open, setOpen] = useState(false);
-  const images = require.context("./Images", true);
+  // const images = require.context("./Images", true);
   const firebaseConfig = {
     apiKey: "AIzaSyA0dberbHig8XFSbGVjV86wf_rO5Eo1C7c",
     authDomain: "wods-53f4c.firebaseapp.com",
@@ -52,16 +56,76 @@ export default function Main() {
   let app = useRef(null);
   let db = useRef(null);
   let wods = useRef(null);
-  let userUid = useRef(null);
-  let userRef = useRef(null);
+  let user = useRef(null);
   const nameRef = useRef("");
-  const emailRef = useRef("");
+  const surnameRef = useRef("");
+  const phoneRef = useRef("");
 
   useEffect(() => {
     app.current = initializeApp(firebaseConfig);
     db.current = getDatabase(app.current);
-    login();
+    authenticate();
   }, []);
+
+  // function getUid() {
+  //   return Math.floor(Math.random() * Date.now()).toString();
+  // }
+
+  async function authenticate() {
+    const auth = getAuth();
+    const authUser = await signInAnonymously(auth);
+    if (authUser) {
+      console.log("UID value:", authUser.user.uid);
+      getWods();
+      user.current = {
+        uid: authUser.user.uid,
+        dbRef: ref(db.current, `users/${authUser.user.uid}`),
+      };
+      getUserData();
+    } else {
+      console.error("Unable to authenticate user");
+    }
+  }
+
+  async function getUserData() {
+    get(user.current.dbRef)
+      .then((userData) => {
+        if (userData.exists()) {
+          //user exists so we use its data
+          const { id, name, surname, phone, workouts } = userData.val();
+          user.current = {
+            id,
+            name,
+            surname,
+            phone,
+            workouts: JSON.parse(workouts),
+          };
+          setIsExistingUser(true);
+          setIsLoading(false);
+        } else {
+          //user doesn't exists, needs to login
+          setIsLoading(false);
+          console.log(`User with id:${user.current.uid} doesn't exist`);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  async function getWods() {
+    const wodsRef = ref(db.current, `workouts`);
+    onValue(wodsRef, (snapshot) => {
+      console.info(snapshot.val());
+      wods.current = snapshot.val();
+      setWorkouts(snapshot.val());
+      // const help = snapshot.val().map((item) => {
+      //   item.uuid = getUid();
+      //   return item;
+      // });
+      // console.log(JSON.stringify(help));
+    });
+  }
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -73,218 +137,183 @@ export default function Main() {
 
   const submitUserData = () => {
     setOpen(false);
-    console.log(nameRef.current.value);
-    console.log(emailRef.current.value);
-    update(userRef.current, {
+    set(user.current.dbRef, {
       name: nameRef.current.value,
-      email: emailRef.current.value,
+      surname: surnameRef.current.value,
+      phone: phoneRef.current.value,
+      id: user.current.uid,
+      workouts: JSON.stringify(wods.current),
     });
+    setIsExistingUser(true);
   };
-
-  async function login() {
-    const auth = getAuth();
-    const authUser = await signInAnonymously(auth);
-    if (authUser) {
-      let existingUser = localStorage.getItem("wod-user_uid");
-      if (!existingUser) {
-        console.log("New user with uid:", authUser.user.uid);
-        localStorage.setItem("wod-user_uid", authUser.user.uid);
-        userUid.current = authUser.user.uid;
-        userRef.current = ref(db.current, `users/${userUid.current}`);
-        set(userRef.current, {
-          name: "dummy",
-          email: "dummy@dummy.com",
-          id: userUid.current,
-          workouts: "{}",
-        });
-      } else {
-        userRef.current = ref(db.current, `users/${existingUser}`);
-        get(userRef.current)
-          .then((userData) => {
-            if (userData.exists()) {
-              //validate is not dummy data
-              if (userData.val().name.includes("dummy")) {
-                //we need to show the modal to get user data
-              } else {
-                getWods();
-              }
-            } else {
-              console.log("No data available");
-            }
-          })
-          .catch((error) => {
-            console.error(error);
-          });
-      }
-    }
-  }
-
-  async function getWods() {
-    wods = ref(db, `workouts`);
-
-    onValue(wods, (snapshot) => {
-      //fires whenever a change occurs
-      let wods = snapshot.val();
-      console.log(wods);
-      setWorkouts(wods);
-    });
-  }
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
 
-  if (isValidUser) {
+  if (isLoading) {
     return (
-      <Grid container spacing={1} justifyContent="center">
-        {workouts.map((wod) => {
-          const { title, description, workout } = wod;
-          return (
-            <Grid item>
-              <Card sx={{ width: 300 }}>
-                <CardHeader
-                  avatar={
-                    <Avatar sx={{ bgcolor: red[900] }} aria-label="recipe">
-                      AH
-                    </Avatar>
-                  }
-                  action={
-                    <IconButton aria-label="add to favorites">
-                      <FavoriteIcon />
-                    </IconButton>
-                  }
-                  title={title}
-                />
-                <CardMedia
-                  component="img"
-                  height="5"
-                  image={images(`./bg.png`)}
-                  alt="Paella dish"
-                />
-                <CardContent>
-                  <Typography
-                    variant="body1"
-                    color="text.primary"
-                    align="center"
-                  >
-                    {description}
-                  </Typography>
-                  {workout.map(function (movement) {
-                    if (movement.effort)
-                      return (
+      <Box sx={{ width: "100%" }}>
+        <LinearProgress />
+      </Box>
+    );
+  } else {
+    if (isExistingUser) {
+      return (
+        <Grid container spacing={1} justifyContent="center">
+          {workouts.map((wod) => {
+            const { title, description, workout } = wod;
+            return (
+              <Grid item>
+                <Card sx={{ width: 300 }}>
+                  <CardHeader
+                    avatar={
+                      <Avatar sx={{ bgcolor: red[900] }} aria-label="recipe">
+                        AH
+                      </Avatar>
+                    }
+                    action={
+                      <IconButton aria-label="add to favorites">
+                        <FavoriteIcon />
+                      </IconButton>
+                    }
+                    title={title}
+                  />
+                  <Divider variant="middle" />
+                  <CardContent>
+                    <Typography
+                      variant="body1"
+                      color="text.primary"
+                      align="center"
+                    >
+                      {description}
+                    </Typography>
+                    {workout.map(function (movement) {
+                      if (movement.effort)
+                        return (
+                          <Typography
+                            variant="body1"
+                            color="text.primary"
+                            align="center"
+                          >
+                            {movement.effort}
+                          </Typography>
+                        );
+                      return wod.type === "ladder" ? (
                         <Typography
                           variant="body1"
                           color="text.primary"
                           align="center"
                         >
-                          {movement.effort}
+                          {`${movement.type} ${
+                            movement.weight ? `@${movement.weight}` : ""
+                          }`}
+                        </Typography>
+                      ) : (
+                        <Typography
+                          variant="body1"
+                          color="text.primary"
+                          align="center"
+                        >
+                          {`${movement.reps} ${movement.type} ${
+                            movement.weight ? `@${movement.weight}` : ""
+                          }`}
                         </Typography>
                       );
-                    return wod.type === "ladder" ? (
-                      <Typography
-                        variant="body1"
-                        color="text.primary"
-                        align="center"
-                      >
-                        {`${movement.type} ${
-                          movement.weight ? `@${movement.weight}` : ""
-                        }`}
-                      </Typography>
-                    ) : (
-                      <Typography
-                        variant="body1"
-                        color="text.primary"
-                        align="center"
-                      >
-                        {`${movement.reps} ${movement.type} ${
-                          movement.weight ? `@${movement.weight}` : ""
-                        }`}
-                      </Typography>
-                    );
-                  })}
-                </CardContent>
-                <CardActions disableSpacing>
-                  <ExpandMore
-                    expand={expanded}
-                    onClick={handleExpandClick}
-                    aria-expanded={expanded}
-                    aria-label="show more"
-                  >
-                    <ExpandMoreIcon />
-                  </ExpandMore>
-                </CardActions>
-                <Collapse in={expanded} timeout="auto" unmountOnExit>
-                  <CardContent>
-                    <Typography paragraph>Method:</Typography>
-                    <Typography paragraph>
-                      Heat 1/2 cup of the broth in a pot until simmering, add
-                      saffron and set aside for 10 minutes.
-                    </Typography>
-                    <Typography paragraph>
-                      Heat oil in a (14- to 16-inch) paella pan or a large, deep
-                      skillet over medium-high heat.
-                    </Typography>
-                    <Typography paragraph>
-                      Add rice and stir very gently to distribute.
-                    </Typography>
-                    <Typography>
-                      Set aside off of the heat to let rest for 10 minutes, and
-                      then serve.
-                    </Typography>
+                    })}
                   </CardContent>
-                </Collapse>
-              </Card>
-            </Grid>
-          );
-        })}
-      </Grid>
+                  <CardActions disableSpacing>
+                    <ExpandMore
+                      expand={expanded}
+                      onClick={handleExpandClick}
+                      aria-expanded={expanded}
+                      aria-label="show more"
+                    >
+                      <ExpandMoreIcon />
+                    </ExpandMore>
+                  </CardActions>
+                  <Collapse in={expanded} timeout="auto" unmountOnExit>
+                    <CardContent>
+                      <Typography paragraph>Method:</Typography>
+                      <Typography paragraph>
+                        Heat 1/2 cup of the broth in a pot until simmering, add
+                        saffron and set aside for 10 minutes.
+                      </Typography>
+                      <Typography paragraph>
+                        Heat oil in a (14- to 16-inch) paella pan or a large,
+                        deep skillet over medium-high heat.
+                      </Typography>
+                      <Typography paragraph>
+                        Add rice and stir very gently to distribute.
+                      </Typography>
+                      <Typography>
+                        Set aside off of the heat to let rest for 10 minutes,
+                        and then serve.
+                      </Typography>
+                    </CardContent>
+                  </Collapse>
+                </Card>
+              </Grid>
+            );
+          })}
+        </Grid>
+      );
+    }
+    return (
+      <div>
+        <Typography align="center" sx={{ m: 10 }}>
+          <Button
+            color="primary"
+            size="large"
+            type="submit"
+            variant="contained"
+            onClick={handleClickOpen}
+          >
+            Login to access the workouts
+          </Button>
+        </Typography>
+        <Dialog open={open} onClose={handleClose}>
+          <DialogTitle>Subscribe</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              To have access to the workouts, please enter your name and email
+              address here.
+            </DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="Name"
+              type="text"
+              fullWidth
+              variant="outlined"
+              inputRef={nameRef}
+            />
+            <TextField
+              margin="dense"
+              id="surname"
+              label="Surname"
+              type="text"
+              fullWidth
+              variant="outlined"
+              inputRef={surnameRef}
+            />
+            <TextField
+              margin="dense"
+              id="name"
+              label="Phone"
+              type="number"
+              fullWidth
+              variant="outlined"
+              inputRef={phoneRef}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button onClick={submitUserData}>Subscribe</Button>
+          </DialogActions>
+        </Dialog>
+      </div>
     );
   }
-  return (
-    <div>
-      <Typography align="center" sx={{ m: 10 }}>
-        <Button
-          color="primary"
-          size="large"
-          type="submit"
-          variant="contained"
-          onClick={handleClickOpen}
-        >
-          Login to access the workouts
-        </Button>
-      </Typography>
-      <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Subscribe</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            To have access to the workouts, please enter your name and email
-            address here.
-          </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Name"
-            type="text"
-            fullWidth
-            variant="standard"
-            inputRef={nameRef}
-          />
-          <TextField
-            margin="dense"
-            id="name"
-            label="Email Address"
-            type="email"
-            fullWidth
-            variant="standard"
-            inputRef={emailRef}
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={submitUserData}>Subscribe</Button>
-        </DialogActions>
-      </Dialog>
-    </div>
-  );
 }
